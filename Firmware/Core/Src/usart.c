badbb67ecb6f7c870610f3ff27ef88b92c0c6a6b
Andrew Xu <badsheep1@Andrews-MacBook-Pro.local>
2026-04-28 11:56:08 -0700

    Fixed registers for clock enables, and enabled GPIOA PA2 and PA3 as AF

--- Firmware/Core/Src/usart.c
+++ Firmware/Core/Src/usart.c
@@ -1,10 +1,13 @@
 #include "usart.h"
 
 USART_STATUS USART_Init(uint32_t baudrate) {
-  /*USART 2 relies on GPIOA pins PA2 and PA3 must configure and enable*/
-  RCC->AHB1ENR |=
-      (1U << 1); // Sets bit 0 high, enables GPIOA AHB1 peripheral clock.
-  RCC->AHB2ENR |= (1U << 17); // Enables USART2 Clock
+  RCC->AHB1ENR |= (1U << 0);  // Enables GPIOA AHB1 peripheral clock.
+  RCC->AHB1ENR |= (1U << 17); // Enables USART2 Clock
+
+  // Enabling GPIO Pins:
+  GPIOA->MODER &= ~(0b1111 << 4); // Clears the PA2 and PA3 bits
+  GPIOA->MODER |=
+      (0b1010 << 4); // Sets both PA2 and PA3 to Alternative Function Mode.
 
   return USART_SUCCESS;
 }
